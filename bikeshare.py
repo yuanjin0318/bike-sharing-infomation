a9b2d00d785b61e5295848723b906c6d75314751
Yuan Jin <yuanjin0318@gmail.com>
2020-08-04 21:23:19 -0400

    Modify the hint reminder

--- bikeshare.py
+++ bikeshare.py
@@ -222,7 +222,7 @@ def main():
         user_stats(df)
         display_data(df)
         
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nHello, would you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
